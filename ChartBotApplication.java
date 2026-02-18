5997585859f66402a3e1efab3d4a6f2f0e4a2f67
Raheem <124266599+skabdulraheem@users.noreply.github.com>
2026-02-18 11:19:07 +0530

    DemoApplication.java

--- ChartBotApplication.java
+++ ChartBotApplication.java
@@ -3,11 +3,10 @@ package com.SpringBoot_AI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ChartBotApplication {
+@SpringBootApplication(scanBasePackages = "com")
+public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChartBotApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
